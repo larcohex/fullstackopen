4d090eec503355d3b1d3b8d8b5f44ae46610d0e7
larcohex <kudayar.pirimbaev@gmail.com>
2020-06-09 21:55:50 +0600

    part 1.7
    * part 1d - Add unicafe, step 2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = () => good + neutral + bad;
+  const average = () => (good - bad) / all();
+  const positive = () => (good / all()) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -17,6 +21,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()} %</p>
     </div>
   );
 };
