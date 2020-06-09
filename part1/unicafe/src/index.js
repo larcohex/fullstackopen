3eedfdbd5aafd40c83182dd57c73fd9d7b8e3a81
larcohex <kudayar.pirimbaev@gmail.com>
2020-06-09 22:08:26 +0600

    part 1.9
    * part 1d - Add unicafe, step 4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,12 +9,18 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {all()}</p>
-      <p>average {average()}</p>
-      <p>positive {positive()} %</p>
+      {all() > 0 ? (
+        <>
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {all()}</p>
+          <p>average {average()}</p>
+          <p>positive {positive()} %</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </>
   );
 };
